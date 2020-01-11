7d13a9bd7310fc4fc9aa274a585309d42dd77004
Samuel Louis <sampeur4life1@gmail.com>
2020-01-11 02:14:57 -0300

    a program that add, remove, calculate the quantity and the price of product in stock

---
+++ ProductInStock/Product.cs
@@ -0,0 +1,36 @@
+﻿using System.Globalization;
+
+namespace ProductInStock
+{
+    class Product
+    {
+        public string Name;
+        public double Price;
+        public int Quantity;
+
+        public double TotalValueInStock()
+        {
+            return Price * Quantity;
+        }
+
+        public void AddProduct(int quantity)
+        {
+            Quantity += quantity;
+        }
+
+        public void RemoveProduct(int quantity)
+        {
+            Quantity -= quantity;
+        }
+
+        public override string ToString()
+        {
+            return Name
+                + ", $ "
+                + Price.ToString("F2", CultureInfo.InvariantCulture)
+                + ", " + Quantity
+                + " Units, Total: $ "
+                + TotalValueInStock().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ ProductInStock/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+
+/// <summary>
+/// a program that add, remove, calculate the quantity and the price of product in stock
+/// </summary>
+
+namespace ProductInStock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product p = new Product();
+
+            Console.WriteLine("Enter product data:");
+            Console.Write("Name: "); p.Name = Console.ReadLine();
+            Console.Write("Price: "); p.Price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantity in stock: ");
+
+            p.Quantity = int.Parse(Console.ReadLine());
+            Console.WriteLine(); Console.WriteLine("Product Data: " + p);
+            Console.WriteLine(); Console.Write("Enter the number of products to add to inventory: ");
+            int qte = int.Parse(Console.ReadLine()); p.AddProduct(qte);
+            Console.WriteLine(); Console.WriteLine("Updated Data: " + p);
+            Console.WriteLine(); Console.Write("Enter the number of products to remove from stock: ");
+            qte = int.Parse(Console.ReadLine()); p.RemoveProduct(qte);
+            Console.WriteLine(); Console.WriteLine("Updated Data: " + p);
+        }
+    }
+}
