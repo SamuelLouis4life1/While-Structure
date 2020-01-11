20f897cfb440d9b64a29c4fcb120638945b535c1
Samuel Louis <sampeur4life1@gmail.com>
2020-01-11 01:04:36 -0300

    a program that calculates the sum of numbers

---
+++ WhileStructure2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace WhileStructure2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, sum;
+
+            x = int.Parse(Console.ReadLine());
+            sum = 0;
+
+            while (x != 0)
+            {
+                sum = sum + x;
+                x = int.Parse(Console.ReadLine());
+
+            }
+            Console.WriteLine("The sum of all numbers is : " + sum);
+            Console.ReadLine();
+        }
+    }
+}
+
