f948f016b04e1e53e7d8fabd0aabb98feda60746
Samuel Louis <sampeur4life1@gmail.com>
2020-01-11 00:52:19 -0300

    inicial commit on my first program about while structure in programming logic

---
+++ WhileStructure1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+/// <summary>
+/// a program that calculates square of a number while that number is not negative.
+/// </summary>
+
+namespace WhileStructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            double x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            while (x >= 0.0)
+            {
+                double raiz = Math.Sqrt(x);
+                Console.WriteLine(raiz.ToString("F3", CultureInfo.InvariantCulture));
+                Console.Write("Enter another number: ");
+                x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            }
+            Console.WriteLine("Negative numbers !");
+        }
+    }
+}
