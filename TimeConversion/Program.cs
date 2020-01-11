e1334b63e02f012e722a6a518ca43954bfbe9407
Samuel Louis <sampeur4life1@gmail.com>
2020-01-11 02:00:49 -0300

    Time Conversion program

---
+++ TimeConversion/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+/// <summary>
+/// Time Conversion
+/// </summary>
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N, hours, minutes, seconds, mod;
+
+            N = int.Parse(Console.ReadLine());
+
+            hours = N / 3600;
+            mod = N % 3600;
+
+            minutes = mod / 60;
+            seconds = mod % 60;
+
+            Console.WriteLine(hours + ":" + minutes + ":" + seconds);
+
+            Console.ReadLine();
+        }
+    }
+}
+
