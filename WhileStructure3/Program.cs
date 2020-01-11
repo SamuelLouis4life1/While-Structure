595a6af4650e21b5c5c64db83fc6f83d9cf39a8c
Samuel Louis <sampeur4life1@gmail.com>
2020-01-11 01:13:13 -0300

    a program that indicates if the values were entered in ascending or decending order

---
+++ WhileStructure3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+// exercise solved URI 1113
+// Read an undetermined number of X and Y integer pairs. 
+// Write a message for each X and Y indicating whether these values ​​were entered in ascending or descending order.
+
+namespace WhileStructure3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int X, Y;
+
+            string[] vect = Console.ReadLine().Split(' ');
+
+            X = int.Parse(vect[0]);
+            Y = int.Parse(vect[1]);
+
+            while (X != Y)
+            {
+                if (X > Y)
+                {
+                    Console.WriteLine("Descending");
+                }
+                else
+                {
+                    Console.WriteLine("Ascending");
+                }
+
+                vect = Console.ReadLine().Split(' ');
+
+                X = int.Parse(vect[0]);
+                Y = int.Parse(vect[1]);
+            }
+        }
+    }
+}
+
